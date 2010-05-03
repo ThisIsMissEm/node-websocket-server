422479605f7a2663d14ee3cd46d31569b13dc72e
Micheil Smith <micheil@brandedcode.com>
2010-05-03 16:59:10 +1000

    Moving the readyStateChange event to the connection, fixed the bug with writing to dead clients.

--- example.js
+++ example.js
@@ -9,15 +9,26 @@ var server = ws.createServer();
 
 server.listen(7000);
 
-server.addListener("readyStateChange", function(readyState){
-  log("stateChanged: "+readyState);
-});
-
 server.addListener("client", function(conn){
   log(conn._id + ": new connection");
+  conn.addListener("readyStateChange", function(readyState){
+    log("stateChanged: "+readyState);
+  });
+  
   
   conn.addListener("open", function(){
     log(conn._id + ": onOpen");
+    server.clients.forEach(function(client){
+      client.write("New Connection: "+conn._id);
+    });
+  });
+  
+  conn.addListener("close", function(){
+    var c = this;
+    log(c._id + ": onClose");
+    server.clients.forEach(function(client){
+      client.write("Connection Closed: "+c._id);
+    });
   });
   
   conn.addListener("message", function(message){
