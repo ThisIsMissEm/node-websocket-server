d56a460a23eb9f3197322da28672153cfd7d72cf
Micheil Smith <micheil@brandedcode.com>
2010-06-10 18:40:41 +1000

    Adding more information about the spam test & adding back in the echoing of messages to the connected clients.

--- examples/echo-server.js
+++ examples/echo-server.js
@@ -29,7 +29,7 @@ server.addListener("connection", function(conn){
   
   conn.addListener("message", function(message){
     log([message.length, JSON.stringify(message)].join(" | "));
-//    server.broadcast("<"+conn._id+"> "+message);
+    server.broadcast("<"+conn._id+"> "+message);
   });
 });
 
