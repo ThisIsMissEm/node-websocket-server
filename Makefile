8046b92636c0d319949750476df6549637e80740
Micheil Smith <micheil@brandedcode.com>
2010-11-02 05:20:00 +1100

    Replacing Rake with Make and adding in infrastructure for testing.

---
+++ Makefile
@@ -0,0 +1,22 @@
+release:
+	node tools/release.js
+
+publish: release
+	npm publish .
+
+test:
+	@echo "Running Simple Tests"
+	@find -f test/simple/test-*.js | xargs -n 1 -t node
+
+test-all: test
+	@echo "Running All Tests"
+	@find -f test/pummel/test-*.js | xargs -n 1 -t node
+
+benchmark: 
+	@echo "Running Benchmarks"
+	@find -f benchmark/simple/*.js | xargs -n 1 -t node
+
+doc:
+	node tools/doctool/doctool.js
+
+.PHONY: release publish test test-all benchmark doc
---
+++ tools/release.js
@@ -0,0 +1,33 @@
+var fs = require("fs")
+  , cp = require("child_process")
+  , cwd = process.cwd;
+
+cp.exec("git describe", function(err, stdout, stderr) {
+  fs.writeFile("package.json", JSON.stringify({ 
+    version: stdout.match(/v([0-9]+\.[0-9]+\.[0-9]+)/)[1],
+    description: "A WebSocket Server for node.js, 90-100% spec compatible.",
+    repository: {
+      type: "git",
+      url: "http://github.com/miksago/node-websocket-server.git"
+    },
+    directories: {
+      doc: "./doc",
+      lib: "./lib/ws/"
+    },
+    main: "./lib/ws/server",
+    bugs: {
+      web: "http://github.com/miksago/node-websocket-server/issues"
+    },
+    author: "Micheil Smith <micheil@brandedcode.com>",
+    licenses: [
+      {
+        type: "MIT",
+        url: "./LICENSE.md"
+      }
+    ],
+    name: "websocket-server",
+    engines: {
+      node: ">=0.2.0-0"
+    }
+  }));
+});
