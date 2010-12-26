80c32d9ab753a7f171aacbef27c73d569290fd25
KensakuKOMATSU <kensaku.komatsu@gmail.com>
2010-12-26 18:45:42 +0900

    Update chat-server.js (w/ nws-memstore)

--- examples/chat-server.js
+++ examples/chat-server.js
@@ -2,7 +2,13 @@ var sys = require("sys")
   , http = require("http")
   , fs = require("fs")
   , path = require("path")
-  , ws = require('../lib/ws/server');
+  , ws = require('../lib/ws/server')
+  , memstore = require('nws-memstore/lib/nws-memstore');
+
+// NOTE::
+// This example requires additional library 'nws-memstore'.
+// It supports per connection memory-storage.
+// see, https://github.com/miksago/nws-memstore
 
 var httpServer = http.createServer(function(req, res){
   if(req.method == "GET"){
@@ -33,33 +39,35 @@ var server = ws.createServer({
   server: httpServer
 });
 
+memstore.attachTo(server);
+
 server.addListener("listening", function(){
   sys.log("Listening for connections.");
 });
 
 // Handle WebSocket Requests
 server.addListener("connection", function(conn){
-  conn.storage.set("username", "user_"+conn.id);
+  conn.memstore.set("username", "user_"+conn.id);
 
   conn.send("** Connected as: user_"+conn.id);
   conn.send("** Type `/nick USERNAME` to change your username");
 
-  conn.broadcast("** "+conn.storage.get("username")+" connected");
+  conn.broadcast("** "+conn.memstore.get("username")+" connected");
 
   conn.addListener("message", function(message){
     if(message[0] == "/"){
       // set username
       if((matches = message.match(/^\/nick (\w+)$/i)) && matches[1]){
-        conn.storage.set("username", matches[1]);
+        conn.memstore.set("username", matches[1]);
         conn.send("** you are now known as: "+matches[1]);
 
       // get message count
       } else if(/^\/stats/.test(message)){
-        conn.send("** you have sent "+conn.storage.get("messages", 0)+" messages.");
+        conn.send("** you have sent "+conn.memstore.get("messages", 0)+" messages.");
       }
     } else {
-      conn.storage.incr("messages");
-      server.broadcast(conn.storage.get("username")+": "+message);
+      conn.memstore.incr("messages");
+      server.broadcast(conn.memstore.get("username")+": "+message);
     }
   });
 });
