d07e0f1efdcba4f67f87a91be9118d7bf2f5d67d
Micheil Smith <micheil@brandedcode.com>
2011-03-25 16:31:46 +1100

    Removed the deprecated storage stuff

--- test/manual/chat-server.js
+++ test/manual/chat-server.js
@@ -2,7 +2,7 @@ var sys = require("sys")
   , http = require("http")
   , fs = require("fs")
   , path = require("path")
-  , ws = require('../lib/ws/server');
+  , ws = require('../../lib/ws/server');
 
 var httpServer = http.createServer(function(req, res){
   if(req.method == "GET"){
@@ -39,28 +39,14 @@ server.addListener("listening", function(){
 
 // Handle WebSocket Requests
 server.addListener("connection", function(conn){
-  conn.storage.set("username", "user_"+conn.id);
 
   conn.send("** Connected as: user_"+conn.id);
   conn.send("** Type `/nick USERNAME` to change your username");
 
-  conn.broadcast("** "+conn.storage.get("username")+" connected");
+  conn.broadcast("** "+conn.id+" connected");
 
   conn.addListener("message", function(message){
-    if(message[0] == "/"){
-      // set username
-      if((matches = message.match(/^\/nick (\w+)$/i)) && matches[1]){
-        conn.storage.set("username", matches[1]);
-        conn.send("** you are now known as: "+matches[1]);
-
-      // get message count
-      } else if(/^\/stats/.test(message)){
-        conn.send("** you have sent "+conn.storage.get("messages", 0)+" messages.");
-      }
-    } else {
-      conn.storage.incr("messages");
-      server.broadcast(conn.storage.get("username")+": "+message);
-    }
+    server.broadcast(conn.id+": "+message);
   });
 });
 
